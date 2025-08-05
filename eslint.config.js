61efdff7fe32c448ed696a9fd729fe55486ea87e
Andreas Nandiwardhana <andreasnw@Andreass-MacBook-Pro-2.local>
2025-08-05 21:56:58 +0700

    chore: configure husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,17 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const typescriptPlugin = require("@typescript-eslint/eslint-plugin");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*"],
+    plugins: {
+      "@typescript-eslint": typescriptPlugin,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error", // Enforce no explicit 'any'
+    },
   },
 ]);
