2dee32ffb5e1a2357c47a4c4fee3158edaa4f77e
Andreas Nandiwardhana <andreasnw@Andreass-MacBook-Pro-2.local>
2025-08-06 15:09:53 +0700

    chore: init navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Navigation from "./app/routes";
 
-const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  );
-};
+const App = () => <Navigation />;
 
 export default App;
-
-const styles = StyleSheet.create({});
